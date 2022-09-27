f9dd684b92f4821a259c9403020dae27769c1770
CrusaderKleptoman <64123771+CrusaderKleptoman@users.noreply.github.com>
2022-09-28 00:54:56 +0800

    Initial commit

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab1_2
+{
+    public struct Square
+    {
+        public int side;
+        public int amount;
+    }
+    public class Logic
+    {
+        public static List<Square> GetSquares(int a, int b)
+        {
+
+            List<Square> squares = new List<Square>();
+
+            int count = 0;
+
+            int maxSide;
+            int minSide;
+            if (a > b)
+            {
+                maxSide = a;
+                minSide = b;
+            }
+            else
+            {
+                maxSide = b;
+                minSide = a;
+            }
+
+            Square square;
+            square.side = minSide;
+            square.amount = 0;
+            squares.Add(square);
+
+            for (int i = 0; i < 1; i = 0)
+            {
+
+                if (maxSide < minSide)
+                {
+                    var swap = maxSide;
+                    maxSide = minSide;
+                    minSide = swap;
+
+                    count++;
+                    square.side = minSide;
+                    square.amount = 0;
+                    squares.Add(square);
+                }
+
+                square.amount++;
+                squares[count] = square;
+                maxSide -= minSide;
+                if (maxSide <= 0 || minSide <= 0)
+                {
+                    break;
+                }
+            }
+
+            return squares;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину сторон прямоугольника");
+
+            Console.WriteLine("a = ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("b = ");
+            int b = int.Parse(Console.ReadLine());
+
+            List<Square> squares = Logic.GetSquares(a, b);
+
+            for (int i = 0; i < squares.Count; i++)
+            {
+                Console.WriteLine("Длина стороны квадрата = " + squares[i].side + "\nИх количество = " + squares[i].amount + "\n");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp3Tests/LogicTests.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using lab1_2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_2.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void GetSquaresTest1()
+        {
+            var a = 100; var b = 30;
+
+            List<Square> squares = new List<Square>();
+            Square square;
+
+            square.side = 30; square.amount = 3;
+            squares.Add(square);
+            square.side = 10; square.amount = 3;
+            squares.Add(square);
+
+            List<Square> message = Logic.GetSquares(a, b);
+
+            String message1 = "";
+            String message2 = "";
+
+            foreach (var item in squares)
+            {
+                message1 = message1 + item.side.ToString() + " ";
+                message1 = message1 + item.amount.ToString() + " ";
+            }
+
+            foreach (var item in message)
+            {
+                message2 = message2 + item.side.ToString() + " ";
+                message2 = message2 + item.amount.ToString() + " ";
+            }
+
+            Assert.AreEqual(message1, message2);
+        }
+
+        [TestMethod()]
+        public void GetSquaresTest2()
+        {
+            var a = 100; var b = 100;
+
+            List<Square> squares = new List<Square>();
+            Square square;
+
+            square.side = 100; square.amount = 1;
+            squares.Add(square);
+
+            List<Square> message = Logic.GetSquares(a, b);
+
+            String message1 = "";
+            String message2 = "";
+
+            foreach (var item in squares)
+            {
+                message1 = message1 + item.side.ToString() + " ";
+                message1 = message1 + item.amount.ToString() + " ";
+            }
+
+            foreach (var item in message)
+            {
+                message2 = message2 + item.side.ToString() + " ";
+                message2 = message2 + item.amount.ToString() + " ";
+            }
+
+            Assert.AreEqual(message1, message2);
+        }
+
+        [TestMethod()]
+        public void GetSquaresTest3()
+        {
+            var a = 25; var b = 132;
+
+            List<Square> squares = new List<Square>();
+            Square square;
+
+            square.side = 25; square.amount = 5;
+            squares.Add(square);
+            square.side = 7; square.amount = 3;
+            squares.Add(square);
+            square.side = 4; square.amount = 1;
+            squares.Add(square);
+            square.side = 3; square.amount = 1;
+            squares.Add(square);
+            square.side = 1; square.amount = 3;
+            squares.Add(square);
+
+            List<Square> message = Logic.GetSquares(a, b);
+
+            String message1 = "";
+            String message2 = "";
+
+            foreach (var item in squares)
+            {
+                message1 = message1 + item.side.ToString() + " ";
+                message1 = message1 + item.amount.ToString() + " ";
+            }
+
+            foreach (var item in message)
+            {
+                message2 = message2 + item.side.ToString() + " ";
+                message2 = message2 + item.amount.ToString() + " ";
+            }
+
+            Assert.AreEqual(message1, message2);
+        }
+    }
+}
